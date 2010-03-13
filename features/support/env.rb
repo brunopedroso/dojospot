9e1210bbcff2fbe26caf48bc0c2c073592db3a4f
Bruno Pedroso <Bruno@macasa.local>
2010-03-12 23:45:25 -0300

    adicionei configuração do watir

--- features/support/env.rb
+++ features/support/env.rb
@@ -55,3 +55,38 @@ if defined?(ActiveRecord::Base)
   rescue LoadError => ignore_if_database_cleaner_not_present
   end
 end
+
+
+# Added to setup watir (folowing http://www.rudyonrails.com/blog/?p=278)
+require 'spec/expectations'
+ 
+if ENV['FIREWATIR']
+  require 'firewatir'
+  Browser = FireWatir::Firefox
+else
+  case RUBY_PLATFORM
+  when /darwin/
+    require 'safariwatir'
+    Browser = Watir::Safari
+  when /win32|mingw/
+    require 'watir'
+    Browser = Watir::IE
+  when /java/
+    require 'celerity'
+    Browser = Celerity::Browser
+  else
+    raise "This platform is not supported (#{PLATFORM})"
+  end
+end
+ 
+# "before all"
+browser = Browser.new
+ 
+Before do
+  @browser = browser
+end
+ 
+# "after all"
+at_exit do
+  browser.close
+end
