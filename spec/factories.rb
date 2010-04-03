e7467b8ea6e12955fd91d71870c6e5f06b536982
Bruno Pedroso <Bruno@macasa.local>
2010-04-03 18:39:37 -0300

    pequeno refact

--- spec/factories.rb
+++ spec/factories.rb
@@ -8,7 +8,7 @@ Factory.define DojoSession do |s|
 		s.title {Factory.next :dojo_session_title}
 		s.text "Venha codar com a gente!"
 		s.place "na SEA tecnologia"
-		s.date {Date.today + 1} # tomorow
+		s.date {Date.today}
 		s.time "17:00 às 19:00"
 end
 
