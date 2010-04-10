449ef4dfbfb4a370abdf8c6efb4b17345f9f684a
Bruno Pedroso <Bruno@macasa.local>
2010-04-10 20:19:15 -0300

    refatorei o sessions_controller sem querer.  - mudei o mecanismo de mock dele, pq naum tava passando no bundle do textmate.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,26 +1,36 @@
 require File.dirname(__FILE__) + '/../spec_helper'
  
 describe SessionsController do
-  # fixtures :all
+
   integrate_views
 
-  it "new action should render new template" do
-    get :new
-    response.should render_template(:new)
-  end
+
+	context 'new action' do
+
+	  it "should render new template" do
+	    get :new
+	    response.should render_template(:new)
+	  end
+
+	end
+
+	context 'create action' do
+		
+	  it "should render new template when authentication is invalid" do
+	    User.stub_chain(:authenticate).and_return(nil)
+	    post :create
+	    response.should render_template(:new)
+	    session['user_id'].should be_nil
+	  end
+
+	  it "should redirect when authentication is valid" do
+			Factory.create :user
+	    User.stub_chain(:authenticate).and_return(User.first)
+	    post :create
+	    response.should redirect_to(root_url)
+	    session['user_id'].should == User.first.id
+	  end
+	
+	end
   
-  it "create action should render new template when authentication is invalid" do
-    User.stubs(:authenticate).returns(nil)
-    post :create
-    response.should render_template(:new)
-    session['user_id'].should be_nil
-  end
-  
-  it "create action should redirect when authentication is valid" do
-		Factory.create :user
-    User.stubs(:authenticate).returns(User.first)
-    post :create
-    response.should redirect_to(root_url)
-    session['user_id'].should == User.first.id
-  end
 end
