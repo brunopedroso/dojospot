a1f1c79cb0b50f038cff9c4a35e28505dda23090
Bruno Pedroso <brunopedroso@gmail.com>
2011-04-10 19:37:15 -0300

    still configuring heroku

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source :gemcutter
 gem 'rails', '2.3.5'
+gem 'pg', :group=>:production
 gem 'RedCloth'
--- config/environment.rb
+++ config/environment.rb
@@ -24,7 +24,7 @@ Rails::Initializer.run do |config|
 	#   config.gem 'rspec-rails', :version => '>= 1.3.2', :lib => false unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
 	# 	config.gem "remarkable_rails", :lib => false
 	
-	config.gem "RedCloth"
+	# config.gem "RedCloth"
 	
 	
   # Only load the plugins named here, in the order given (default is alphabetical).
