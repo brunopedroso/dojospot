a41c385b8a2b7cf82a1037718d9bccab27fbc107
Bruno Pedroso <brunopedroso@gmail.com>
2011-04-21 20:29:07 -0300

    finished all participants' page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,6 +3,7 @@ require 'digest/md5'
 module UsersHelper
 	
 	def gravatarUrlFor(email, options={})
+		email = '' if email.nil?
 		hash = Digest::MD5.hexdigest(email.downcase)
 		"http://www.gravatar.com/avatar/#{hash}.png?s=#{options[:size]}"
 	end
