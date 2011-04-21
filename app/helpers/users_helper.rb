d8f73dad0a1ce9d0b786c4337e591321040b0bf0
Bruno Pedroso <brunopedroso@gmail.com>
2011-04-21 11:36:48 -0300

    adjusted a minimum the layout of the gravatar in the confirmed users list

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,9 +2,9 @@ require 'digest/md5'
 
 module UsersHelper
 	
-	def gravatarUrlFor(email)
+	def gravatarUrlFor(email, options={})
 		hash = Digest::MD5.hexdigest(email.downcase)
-		"http://www.gravatar.com/avatar/#{hash}.png"
+		"http://www.gravatar.com/avatar/#{hash}.png?s=#{options[:size]}"
 	end
 	
 end
