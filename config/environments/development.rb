ff6c90411d4bed9ffd346db3618e8e5418647d38
Bruno Pedroso <brunopedroso@gmail.com>
2012-03-31 11:26:29 -0300

    still

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-DojospotRails3::Application.configure do
+Dojospot::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
